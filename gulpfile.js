58eedeeac96ec55f43bc0a8a9f4c605ec60cafd7
farhad-rad <farhad.rad.official@gmail.com>
2024-09-28 14:57:44 +0330

    use gulp for better build process

---
+++ gulpfile.js
@@ -0,0 +1,36 @@
+const gulp = require("gulp");
+const clean = require("gulp-clean");
+const run = require("gulp-run");
+const fs = require("fs");
+
+gulp.task("clear", function () {
+  if (fs.existsSync("dist")) {
+    return gulp
+      .src("dist/*", {
+        read: false,
+      })
+      .pipe(clean());
+  }
+  return gulp.src(".");
+});
+
+gulp.task("compile", function () {
+  return run("npm run server:build", {
+    silent: false,
+    verbosity: 3,
+  }).exec();
+});
+
+gulp.task("move", function () {
+  return gulp.src("server/dist/**/*").pipe(gulp.dest("dist"));
+});
+
+gulp.task("finish-up", function () {
+  return gulp
+    .src("server/dist/*", {
+      read: false,
+    })
+    .pipe(clean());
+});
+
+gulp.task("build", gulp.series("clear", "compile", "move", "finish-up"));
